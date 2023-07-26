e7e15a4f86dc33447259720aa6e662895ef608a0
Akshay <akshaypanampatta8891@gmail.com>
2023-07-26 20:30:56 +0530

    added productslist

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,32 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Card } from 'react-bootstrap';
-import "./Products.css"
+import React, { useEffect, useState } from "react";
+import "./Products.css";
+import { Button, Card } from "react-bootstrap";
+import "./Products.css";
+
 
 const Products = () => {
-    const [products,setProducts]=useState([]);
-    useEffect(()=>{
-        const fetchProducts = async()=>{
-            const response = await fetch("https://fakestoreapi.com/products");
-            const data = await response.json()
-            console.log(data);
-            setProducts(data)
-        }
-        fetchProducts()
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
 
-    },[])
+
   return (
-    <div>
+    <div className="products">
       <div className="container">
-        <div className='row'>
-          <div className='col-12 '>
-            <h1 className='text-center  mt-4'>Latest Products</h1>
-
+        <div className="row">
+          <div className="col-12 ">
+            <h1 className="latest-products text-center fw-bold mt-3">
+              Latest Products
+            </h1>
           </div>
-
+        </div>
+        <div className="row mt-3 mb-5">
+          <div className="ctgry-btn col-12  ">
+            <Button variant="dark m-1">All</Button>
+            <Button variant="dark m-1">Jewellery</Button>
+            <Button variant="dark m-1">Men's Clothing</Button>
+            <Button variant="dark m-1">Women's Clothing</Button>
+            <Button variant="dark m-1 ">Electronic</Button>
+          </div>
+        </div>
+        <div className="row ">
+          {products.map((product) => (
+            <div className="col-lg-3 col-md-4 col-sm-6  mb-4 d-flex justify-content-center ">
+              <Card style={{ width: "15rem" }}>
+                <Card.Img variant="top" src={product.image} height="250px" />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text className="text-center fw-bold">
+                    ${product.price}
+                  </Card.Text>
+                  <div className="d-flex justify-content-center">
+                    <Button variant="dark">Buy Now</Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Products
+export default Products;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
-import Slider from '../Components/Home/Slider'
+import Slider from '../Components/Slider/Slider'
 import Products from '../Components/Products/Products'
 
 const HomePage = () => {
